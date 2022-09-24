a49818509f290f914f9ec29bd45c342e59b7f5de
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-09-24 23:19:14 +0100

    ok

---
+++ src/test/java/com/unicus/services/UserServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.unicus.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
